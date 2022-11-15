f09e9b6c28ca4bf69db8f425e79f5cf37737601e
Robin98123 <robinson1998sena@gmail.com>
2022-11-15 09:24:40 -0500

    se suben los cambios hechos por Brayan y por Robinson al html creando el canvas y dandole estilos

---
+++ scripts/tetris.js
@@ -0,0 +1,20 @@
+let lasTime = 0;
+
+const canvas = document.getElementById('tetris');
+const context = canvas.getContext('2d');
+
+
+context.scale(20,20);
+
+function draw (){
+    context.fillStyle = "#000";
+    context.fillRect (0, 0, canvas.width, canvas.height);
+}
+
+function update(time = 0) {
+    const deltaTime = time - lastTime;
+    lastTime = time;
+    requestAnimationFrame (update);
+}
+
+update();
